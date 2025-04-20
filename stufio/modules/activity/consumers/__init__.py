be9588ab8b681579eb92d20618331338a9a38f6d
IF <i.finchuk@gmail.com>
2025-04-20 18:02:47 +0200

    Consumers config fix

--- stufio/modules/activity/consumers/__init__.py
+++ stufio/modules/activity/consumers/__init__.py
@@ -1,2 +1,8 @@
-# Import all consumers to ensure they're registered
-from .activity_consumer import handle_user_activity
+from stufio.core.config import get_settings
+
+settings = get_settings()
+
+if settings.events_APP_CONSUME_ROUTES:
+
+    # Import all consumers to ensure they're registered
+    from .activity_consumer import handle_user_activity
